a4e34bac901ff8761c09ac5cd716fb06bad1e992
Torrey Payne <torreyl.payne@gmail.com>
2015-05-08 17:19:25 -0700

    Begin Phase IV

--- w3d5/skeleton/lib/03_associatable.rb
+++ w3d5/skeleton/lib/03_associatable.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative '02_searchable'
 require 'active_support/inflector'
 
@@ -20,23 +21,74 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @name = name
+    options[:foreign_key] ||= (name.to_s.singularize + "_id").to_sym
+    options[:primary_key] ||= :id
+    options[:class_name] ||= name.capitalize
+    # debugger
+    @foreign_key, @primary_key = options[:foreign_key], options[:primary_key]
+    @class_name = options[:class_name]
+  end
+
+  def model_class
+    @class_name.to_s.constantize
+  end
+
+  def table_name
+    (@class_name + "s").downcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
+    options[:foreign_key] ||= (self_class_name.to_s.downcase + "_id").to_sym
+    options[:primary_key] ||= :id
+    options[:class_name] ||= name.to_s.singularize.capitalize
+    @foreign_key, @primary_key = options[:foreign_key], options[:primary_key]
+    @class_name = options[:class_name]
     # ...
+  end
+
+  def model_class
+    @class_name.constantize
+  end
+
+  def table_name
+    (@class_name + "s").downcase
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      # debugger
+      key = self.send(options.foreign_key)
+      target_model = options.model_class
+      where_cond = Hash.new
+      where_cond[options.primary_key.to_sym] = key
+      result = target_model.where(where_cond).first
+    end
     # ...
   end
 
   def has_many(name, options = {})
+    options = HasManyOptions.new(name, self, options)
+
+    define_method(name) do
+
+      key = self.send(options.primary_key)
+      target_model = options.model_class
+      where_cond = Hash.new
+      # debugger
+      where_cond[options.foreign_key.to_sym] = key
+
+      result = target_model.where(where_cond)
+
+    end
+
     # ...
   end
 
@@ -46,5 +98,6 @@ module Associatable
 end
 
 class SQLObject
+  extend Associatable
   # Mixin Associatable here...
 end
--- w3d5/skeleton/lib/04_associatable2.rb
+++ w3d5/skeleton/lib/04_associatable2.rb
@@ -5,6 +5,8 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
+    # Needs to link to the ONE middle association that links to ONE further assoc.
+    option1 = belongs_to(through_name.to_s.constantize.source_name)
     # ...
   end
 end
