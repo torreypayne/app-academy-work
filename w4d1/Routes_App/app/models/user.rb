5d45a24ad5b81c63ff74e66fdbcf9291c936ede9
App Academy Student <student@appacademy.io>
2015-05-11 21:28:08 -0700

    Finish Contacts App

--- w4d1/Routes_App/app/models/user.rb
+++ w4d1/Routes_App/app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ActiveRecord::Base
-  has_many(
-  :contacts,
-  class_name: 'Contact',
-  foreign_key: :user_id,
-  primary_key: :id
-  )
+  has_many :contacts, dependent: :destroy
 
-  has_many :contact_shares
+  has_many :contact_shares, dependent: :destroy
+
   has_many(
   :shared_contacts,
   through: :contact_shares,
