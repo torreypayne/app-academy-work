1634b5ef2b00606405ce7ec1981e60596e1c62c6
App Academy Student <student@appacademy.io>
2015-05-14 12:03:11 -0700

    Complete warmup phase; Finish associations

---
+++ w4d4/Music_App/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+
+  def create
+    new_user = User.new(user_params)
+    if new_user.save!
+      log_in_user!(new_user)
+      redirect_to user_url(new_user)
+    else
+      flash[:errors] << new_user.errors.full_messages
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def new
+    new_user = User.new
+  end
+
+  def show
+  end
+
+  def index
+    render json: User.all
+  end
+end
