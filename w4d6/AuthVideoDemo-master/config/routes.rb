707d4dd92e86108ab1ecc344c4cc2c4ea0a63ac5
Torrey Payne <torreyl.payne@gmail.com>
2015-05-17 18:46:18 -0700

    Finish through Phase 5.2

--- w4d6/AuthVideoDemo-master/config/routes.rb
+++ w4d6/AuthVideoDemo-master/config/routes.rb
@@ -1,6 +1,11 @@
 AuthDemo::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/contact"
+  post "static_pages/contact"
   resource :session, only: [:create, :destroy, :new]
   resource :user, only: [:create, :new, :show] do
     resource :counter, only: [:update]
   end
+  resources :static_pages, only: [:home, :contact, :about]
 end
