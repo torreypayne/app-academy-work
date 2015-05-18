ca442e29653ca114421cec18e2cb917e7b0276c5
Torrey Payne <torreyl.payne@gmail.com>
2015-05-18 16:00:56 -0700

    Add FactoryGirl And Faker object to Specs

--- w5d1/goal_app/app/controllers/application_controller.rb
+++ w5d1/goal_app/app/controllers/application_controller.rb
@@ -2,4 +2,35 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :ensure_logged_in, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def log_in!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def log_out!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def ensure_logged_in
+    unless current_user
+      redirect_to new_session_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
